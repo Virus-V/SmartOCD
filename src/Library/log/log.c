ab6b62f516a8e688e384faf9124a5d04406adc3f
Virus.V <virusv@live.com>
2023-05-13 17:05:11 +0800

    简化Log系统

--- src/Library/log/log.c
+++ src/Library/log/log.c
@@ -1,133 +1,54 @@
-/*
- * Copyright (c) 2017 rxi
+/**
+ * Copyright (c) 2023, Virus.V <virusv@live.com>
+ * All rights reserved.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- */
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of SmartOCD nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright 2023 Virus.V <virusv@live.com>
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
-#include <string.h>
+#include <unistd.h>
 #include <time.h>
 
 #include "log.h"
 
-static struct {
-  void *udata;
-  log_LockFn lock;
-  FILE *fp;
-  int level;
-  int quiet;
-} L;
+int g_log_level = LOG_INFO;
+int g_log_fd = STDERR_FILENO;
 
-
-static const char *level_names[] = {
-  "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"
-};
-
-#ifdef LOG_USE_COLOR
-static const char *level_colors[] = {
-  "\x1b[94m", "\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m", "\x1b[35m"
-};
-#endif
-
-
-static void lock(void)   {
-  if (L.lock) {
-    L.lock(L.udata, 1);
-  }
-}
-
-
-static void unlock(void) {
-  if (L.lock) {
-    L.lock(L.udata, 0);
-  }
-}
-
-
-void log_set_udata(void *udata) {
-  L.udata = udata;
-}
-
-
-void log_set_lock(log_LockFn fn) {
-  L.lock = fn;
-}
-
-
-void log_set_fp(FILE *fp) {
-  L.fp = fp;
-}
-
-
-void log_set_level(int level) {
-  L.level = level;
-}
-
-
-void log_set_quiet(int enable) {
-  L.quiet = enable ? 1 : 0;
-}
-
-
-void log_log(int level, const char *file, int line, const char *fmt, ...) {
-  if (level < L.level) {
-    return;
-  }
-
-  /* Acquire lock */
-  lock();
-
+void log_print_time(int fd) {
   /* Get current time */
   time_t t = time(NULL);
   struct tm *lt = localtime(&t);
-  /* Log to stderr */
-  if (!L.quiet) {
-    va_list args;
-    char buf[16];
-    buf[strftime(buf, sizeof(buf), "%H:%M:%S", lt)] = '\0';
+  char buf[32];
+
+  buf[strftime(buf, sizeof(buf), "%H:%M:%S", lt)] = '\0';
 #ifdef LOG_USE_COLOR
-    fprintf(
-      stderr, "%s %s%-5s\x1b[0m \x1b[34m%s:%d:\x1b[0m ",
-      buf, level_colors[level], level_names[level], file, line);
+  dprintf(fd, "\x1b[36m%s\x1b[0m ", buf);
 #else
-    fprintf(stderr, "%s %-5s %s:%d: ", buf, level_names[level], file, line);
+  dprintf(fd, "%s ", buf);
 #endif
-    va_start(args, fmt);
-    vfprintf(stderr, fmt, args);
-    va_end(args);
-    fprintf(stderr, "\n");
-  }
-
-  /* Log to file */
-  if (L.fp) {
-    va_list args;
-    char buf[32];
-    buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", lt)] = '\0';
-    fprintf(L.fp, "%s %-5s %s:%d: ", buf, level_names[level], file, line);
-    va_start(args, fmt);
-    vfprintf(L.fp, fmt, args);
-    va_end(args);
-    fprintf(L.fp, "\n");
-  }
-
-  /* Release lock */
-  unlock();
 }
--- src/Library/log/log.h
+++ src/Library/log/log.h
@@ -1,51 +1,160 @@
 /**
- * Copyright (c) 2017 rxi
+ * Copyright (c) 2023, Virus.V <virusv@live.com>
+ * All rights reserved.
  *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the MIT license. See `log.c` for details.
- */
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of SmartOCD nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright 2023 Virus.V <virusv@live.com>
+*/
 
 #ifndef LOG_H
 #define LOG_H
 
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include <string.h>
-
-// Windows 下面使用 '\\' 替换 '/'
-#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
 #include "smartocd.h"
 
-#define LOG_VERSION "0.1.0"
+/* 全局log等级 */
+extern int g_log_level;
+/* 默认日志输出 */
+extern int g_log_fd;
+void log_print_time(int fd);
 
-typedef void (*log_LockFn)(void *udata, int lock);
-
-enum { LOG_TRACE = 0,
-       LOG_DEBUG,
-       LOG_INFO,
-       LOG_WARN,
-       LOG_ERROR,
-       LOG_FATAL,
+enum {
+  LOG_FATAL = 0,
+  LOG_ERROR,
+  LOG_WARN,
+  LOG_INFO,
+  LOG_DEBUG,
+  LOG_TRACE,
 };
 
-#define log_trace(...) log_log(LOG_TRACE, __FILENAME__, __LINE__, __VA_ARGS__)
-#define log_debug(...) log_log(LOG_DEBUG, __FILENAME__, __LINE__, __VA_ARGS__)
-#define log_info(...) log_log(LOG_INFO, __FILENAME__, __LINE__, __VA_ARGS__)
-#define log_warn(...) log_log(LOG_WARN, __FILENAME__, __LINE__, __VA_ARGS__)
-#define log_error(...) log_log(LOG_ERROR, __FILENAME__, __LINE__, __VA_ARGS__)
-#define log_fatal(...)                                       \
-  do {                                                       \
-    log_log(LOG_FATAL, __FILENAME__, __LINE__, __VA_ARGS__); \
-    abort();                                                 \
+#define LOG_COLOR_TRACE "\x1b[94m"
+#define LOG_COLOR_DEBUG "\x1b[36m"
+#define LOG_COLOR_INFO "\x1b[32m"
+#define LOG_COLOR_WARN "\x1b[33m"
+#define LOG_COLOR_ERROR "\x1b[31m"
+#define LOG_COLOR_FATAL "\x1b[35m"
+
+#ifdef LOG_USE_COLOR
+#define log_trace(fmt, ...)                                                    \
+  if (g_log_level >= LOG_TRACE) {                                              \
+    log_print_time(g_log_fd);                                                  \
+    dprintf(g_log_fd,                                                          \
+            LOG_COLOR_TRACE "TRACE\x1b[0m \x1b[34m%s:%d|\x1b[0m " fmt "\r\n",  \
+            __func__, __LINE__, ##__VA_ARGS__);                                \
+  }
+
+#define log_debug(fmt, ...)                                                    \
+  if (g_log_level >= LOG_DEBUG) {                                              \
+    log_print_time(g_log_fd);                                                  \
+    dprintf(g_log_fd,                                                          \
+            LOG_COLOR_DEBUG "DEBUG\x1b[0m \x1b[34m%s:%d|\x1b[0m " fmt "\r\n",  \
+            __func__, __LINE__, ##__VA_ARGS__);                                \
+  }
+
+#define log_info(fmt, ...)                                                     \
+  if (g_log_level >= LOG_INFO) {                                               \
+    log_print_time(g_log_fd);                                                  \
+    dprintf(g_log_fd,                                                          \
+            LOG_COLOR_INFO "INFO\x1b[0m \x1b[34m%s:%d|\x1b[0m " fmt "\r\n",    \
+            __func__, __LINE__, ##__VA_ARGS__);                                \
+  }
+
+#define log_warn(fmt, ...)                                                     \
+  if (g_log_level >= LOG_WARN) {                                               \
+    log_print_time(g_log_fd);                                                  \
+    dprintf(g_log_fd,                                                          \
+            LOG_COLOR_WARN "WARN\x1b[0m \x1b[34m%s:%d|\x1b[0m " fmt "\r\n",    \
+            __func__, __LINE__, ##__VA_ARGS__);                                \
+  }
+
+#define log_error(fmt, ...)                                                    \
+  if (g_log_level >= LOG_ERROR) {                                              \
+    log_print_time(g_log_fd);                                                  \
+    dprintf(g_log_fd,                                                          \
+            LOG_COLOR_ERROR "ERROR\x1b[0m \x1b[34m%s:%d|\x1b[0m " fmt "\r\n",  \
+            __func__, __LINE__, ##__VA_ARGS__);                                \
+  }
+
+#define log_fatal(fmt, ...)                                                    \
+  do {                                                                         \
+    log_print_time(g_log_fd);                                                  \
+    dprintf(g_log_fd,                                                          \
+            LOG_COLOR_FATAL "FATAL\x1b[0m \x1b[34m%s(%s):%d|\x1b[0m " fmt      \
+                            "\r\n",                                            \
+            __FILE__, __func__, __LINE__, ##__VA_ARGS__);                      \
+    abort();                                                                   \
   } while (0);
 
-void log_set_udata(void *udata);
-void log_set_lock(log_LockFn fn);
-void log_set_fp(FILE *fp);
-void log_set_level(int level);
-void log_set_quiet(int enable);
+#else /* LOG_USE_COLOR */
+#define log_trace(fmt, ...)                                                    \
+  if (g_log_level >= LOG_TRACE) {                                              \
+    log_print_time(g_log_fd);                                                  \
+    dprintf(g_log_fd, "TRACE %s:%d| " fmt "\r\n", __func__, __LINE__,          \
+            ##__VA_ARGS__);                                                    \
+  }
 
-void log_log(int level, const char *file, int line, const char *fmt, ...);
+#define log_debug(fmt, ...)                                                    \
+  if (g_log_level >= LOG_DEBUG) {                                              \
+    log_print_time(g_log_fd);                                                  \
+    dprintf(g_log_fd, "DEBUG %s:%d| " fmt "\r\n", __func__, __LINE__,          \
+            ##__VA_ARGS__);                                                    \
+  }
+
+#define log_info(fmt, ...)                                                     \
+  if (g_log_level >= LOG_INFO) {                                               \
+    log_print_time(g_log_fd);                                                  \
+    dprintf(g_log_fd, "INFO %s:%d| " fmt "\r\n", __func__, __LINE__,           \
+            ##__VA_ARGS__);                                                    \
+  }
+
+#define log_warn(fmt, ...)                                                     \
+  if (g_log_level >= LOG_WARN) {                                               \
+    log_print_time(g_log_fd);                                                  \
+    dprintf(g_log_fd, "WARN %s:%d| " fmt "\r\n", __func__, __LINE__,           \
+            ##__VA_ARGS__);                                                    \
+  }
+
+#define log_error(fmt, ...)                                                    \
+  if (g_log_level >= LOG_ERROR) {                                              \
+    log_print_time(g_log_fd);                                                  \
+    dprintf(g_log_fd, "ERROR %s:%d| " fmt "\r\n", __func__, __LINE__,          \
+            ##__VA_ARGS__);                                                    \
+  }
+
+#define log_fatal(fmt, ...)                                                    \
+  do {                                                                         \
+    log_print_time(g_log_fd);                                                  \
+    dprintf(g_log_fd, "FATAL %s(%s):%d| " fmt "\r\n", __FILE__, __func__,      \
+            __LINE__, ##__VA_ARGS__);                                          \
+    abort();                                                                   \
+  } while (0);
+
+#endif /* LOG_USE_COLOR */
 
 #endif
